d824fcd06ecc09047746653975cd7246019e9ec8
John Lewis <johnbchron@outlook.com>
2019-11-21 17:15:54 -0600

    Update hello_world_halfsize_st7735.ino

--- unusable_experiment_demos/hello_world_halfsize_st7735/hello_world_halfsize_st7735.ino
+++ unusable_experiment_demos/hello_world_halfsize_st7735/hello_world_halfsize_st7735.ino
@@ -2,6 +2,7 @@
 #include "Ucglib.h"
 
 Ucglib_ST7735_18x128x160_HWSPI ucg(/*cd=*/ 7, /*cs=*/ 8, /*reset=*/ 6);
+String currentTime = "0000";
 
 
 void setup(void){
@@ -14,10 +15,23 @@ void setup(void){
 }
 
 void loop(void){  
+  ucg.setColor(255, 255, 0);
   ucg.setPrintPos(5, 5 + 29);
   ucg.print("It's smart watch time!");
+  ucg.setColor(255, 255, 255);
   ucg.setPrintPos(10, 15 + 29);
   ucg.print("Don't worry,");
   ucg.setPrintPos(10, 25 + 29);
   ucg.print("the screen will be bigger.");
+  ucg.setPrintPos(10, 40 + 29);
+  ucg.print("Seconds from launch:");
+  
+  ucg.setPrintPos(10, 55 + 29);
+  ucg.setColor(0, 0, 0);
+  ucg.print(currentTime);
+  
+  ucg.setPrintPos(10, 55 + 29);
+  ucg.setColor(255, 255, 255);
+  currentTime = String(int(millis()/1000));
+  ucg.print(currentTime);
 }
