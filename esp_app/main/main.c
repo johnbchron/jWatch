0477693093d4fe3a7049a9faeedc39b4e94a6f2a
John Lewis <johnbchron@outlook.com>
2020-02-16 12:34:35 -0600

    Update main.c to clean stuff out and add a bunch of comments

--- esp_app/main/main.c
+++ esp_app/main/main.c
@@ -1,14 +1,20 @@
+
+// this is included so that we can output to the serial stream (printf() and stuff)
 #include <stdio.h>
+// this is included so that we can use native logging commands (ESP_LOG_WRITE(), in longhand)
+#include <esp_log.h>
+// these next two are included so that we can run scheduled tasks for things like BLE scanning
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+// Aight, I'm excited to start making this thing!
 
-void app_main(void)
-{
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+void app_main(void){
+    // So this is the app's "entry point"; this is where the actual, real, sequential execution
+    //   of code begins. Once the ESP32 boots, it immediately begins executing from here; this
+    //   main function can make references to whatever it needs to, but here is where it starts.
+
+    printf("Hello, World! Welcome to the ESP32!\n");
+
+    // As of right now, if we leave the end of this function empty... it'll just hang. This is fine.
 }
