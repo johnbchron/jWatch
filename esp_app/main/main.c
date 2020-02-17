81324e02361a6d9031cc1a0a90de0d8bc1c83849
John Lewis <johnbchron@outlook.com>
2020-02-17 12:57:36 -0600

    Updated main.c and added comments

--- esp_app/main/main.c
+++ esp_app/main/main.c
@@ -1,7 +1,7 @@
 
 // this is included so that we can output to the serial stream (printf() and stuff)
 #include <stdio.h>
-// this is included so that we can use native logging commands (ESP_LOG_WRITE(), in longhand)
+// this is included so that we can use native logging commands (ESP_LOG*(), in longhand)
 #include <esp_log.h>
 // these next two are included so that we can run scheduled tasks for things like BLE scanning
 #include "freertos/FreeRTOS.h"
@@ -14,7 +14,11 @@ void app_main(void){
     //   of code begins. Once the ESP32 boots, it immediately begins executing from here; this
     //   main function can make references to whatever it needs to, but here is where it starts.
 
-    printf("Hello, World! Welcome to the ESP32!\n");
+    // printf("Hello, World! Welcome to the ESP32!\n"); outputs:
+    //   "Hello, World! Welcome to the ESP32!", whereas
+    // ESP_LOGI("meta", "Hello, World! Welcome to the ESP32!"); outputs:
+
+    ESP_LOGI("meta", "Hello, World! Welcome to the ESP32!");
 
     // As of right now, if we leave the end of this function empty... it'll just hang. This is fine.
 }
